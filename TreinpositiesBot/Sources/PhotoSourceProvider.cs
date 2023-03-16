0e1a7b8579f439e246a21cf6e58942a2858057cd
Dirk Kok <the@dirkkok.nl>
2023-03-16 09:40:37 +0100

    handle empty or missing SourcesByGuild object

--- TreinpositiesBot/Sources/PhotoSourceProvider.cs
+++ TreinpositiesBot/Sources/PhotoSourceProvider.cs
@@ -14,7 +14,7 @@ public class PhotoSourceProvider {
 
 	public List<PhotoSource> GetPhotoSources(DiscordChannel channel) {
 		List<PhotoSource> ret;
-		if (channel.GuildId.HasValue && m_Config.CurrentValue.SourcesByGuild.TryGetValue(channel.GuildId.Value, out List<string>? sourceNames)) {
+		if (channel.GuildId.HasValue && m_Config.CurrentValue.SourcesByGuild != null && m_Config.CurrentValue.SourcesByGuild.TryGetValue(channel.GuildId.Value, out List<string>? sourceNames)) {
 			ret = m_Sources.Where(source => sourceNames.Contains(source.Name)).ToList();
 			return ret;
 		} else {
